9a14f629d63e1719fb2224d4827c476917b506fd
ankit <ankit@MacBook-Air.local>
2024-04-06 23:02:36 +0530

    add default function for string and numeric

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env::Args;
 use std::error::Error;
 use std::any::Any;
 use std::io;
@@ -74,6 +75,78 @@ fn default_numeric_function(m: &mut HashMap<function_name, Function>) -> &mut Ha
             Err(e) => Err(e)
         }
     }));
+
+    m.insert(function_name::GreaterEq, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::GreaterEq, &input, &args_refs, 1) {
+            Ok((num, args_no)) =>  Ok(num >= args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+    m.insert(function_name::Lower, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::Lower, &input, &args_refs, 1) {
+            Ok((num, args_no)) =>  Ok(num < args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+    m.insert(function_name::LowerEq, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::LowerEq, &input, &args_refs, 1) {
+            Ok((num, args_no)) =>  Ok(num <= args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+    m.insert(function_name::Between, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::Between, &input, &args_refs, 2) {
+            Ok((num, args_no)) =>  Ok(num > args_no[0] && num < args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+    
+    m.insert(function_name::BetweenEq, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::BetweenEq, &input, &args_refs, 2) {
+            Ok((num, args_no)) =>  Ok(num >= args_no[0] && num <= args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+    m.insert(function_name::NotBetween, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::Between, &input, &args_refs, 2) {
+            Ok((num, args_no)) =>  Ok(num <= args_no[0] || num >= args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+    m.insert(function_name::NotBetweenEq, Box::new(|input: Box<dyn Any>, args: Vec<Box<dyn Any>>| {
+        let args_refs: Vec<&dyn Any> = args.iter().map(|arg| arg.as_ref()).collect();
+
+        match parse_numeric(function_name::NotBetweenEq, &input, &args_refs, 2) {
+            Ok((num, args_no)) =>  Ok(num > args_no[0] && num < args_no[0]),
+            Err(e) => Err(e)
+            
+        }
+    }));
+
+
     m
 
 }
@@ -126,3 +199,105 @@ fn parse_numeric( function_name: function_name ,
 
 
 
+fn default_string_function(m: &mut HashMap<function_name, Function>)  -> &mut HashMap<function_name, Function> {
+    m.insert(function_name::EqualIgnoreCase, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("EqualIgnoreCase: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.eq_ignore_ascii_case(arg_str))
+    }));
+
+    m.insert(function_name::EqualAnyIgnoreCase, Box::new(|input, args| {
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        for arg in args {
+            if let Some(arg_str) = arg.downcast_ref::<String>() {
+                if input_str.eq_ignore_ascii_case(arg_str) {
+                    return Ok(true);
+                }
+            }
+        }
+        Ok(false)
+    }));
+
+    m.insert(function_name::EqualAny, Box::new(|input, args| {
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        for arg in args {
+            if let Some(arg_str) = arg.downcast_ref::<String>() {
+                if input_str == arg_str {
+                    return Ok(true);
+                }
+            }
+        }
+        Ok(false)
+    }));
+
+    m.insert(function_name::NotEqualAny, Box::new(|input, args| {
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        for arg in args {
+            if let Some(arg_str) = arg.downcast_ref::<String>() {
+                if input_str == arg_str {
+                    return Ok(false);
+                }
+            }
+        }
+        Ok(true)
+    }));
+
+    m.insert(function_name::StartsWith, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("StartsWith: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.starts_with(arg_str))
+    }));
+
+    m.insert(function_name::StartsWithIgnoreCase, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("StartsWith: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.to_lowercase().starts_with(&arg_str.to_lowercase()))
+    }));
+
+    m.insert(function_name::EndsWith, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("EndsWith: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.ends_with(arg_str))
+    }));
+
+    m.insert(function_name::EndsWithIgnoreCase, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("EndsWithIgnoreCase: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.to_lowercase().ends_with(&arg_str.to_lowercase()))
+    }));
+
+    m.insert(function_name::Contains, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("EndsWithIgnoreCase: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.contains(arg_str))
+    }));
+    m.insert(function_name::ContainsIgnoreCase, Box::new(|input, args| {
+        if args.len() != 1 {
+            return Err("EndsWithIgnoreCase: needs one argument".into());
+        }
+        let input_str = input.downcast_ref::<String>().ok_or("Input is not a string")?;
+        let arg_str = args[0].downcast_ref::<String>().ok_or("Argument is not a string")?;
+        Ok(input_str.to_lowercase().contains(&arg_str.to_lowercase()))
+    }));
+    
+    m
+
+}
